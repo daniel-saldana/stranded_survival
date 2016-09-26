7bfba285c94524555f2f652c18afae01e0c59070
Daniel <daniel.e.saldana@gmail.com>
2016-09-25 17:42:34 -0700

    ok, now this should be all good. Health and energy and sprint working

---
+++ Don'tDie/Assets/Scripts/Fruit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fruit : MonoBehaviour
+{
+
+    public int healthRaw = 1;
+    //Health gained from consuming Fruit.
+    public int healthCooked = 3;
+    //Health gained from consuming Fruit in base.
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ Don'tDie/Assets/Scripts/Meat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meat : MonoBehaviour
+{
+
+    public int healthRaw = 2;
+    //Health gained from consuming Meat
+    public int healthCooked = 5;
+    //Health gained from consuming Meat in base
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+}
